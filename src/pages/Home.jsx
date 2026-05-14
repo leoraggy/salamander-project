8c509291378b2286ccb1f9b96197062c8ccce328
leoraggy <leoragual35@gmail.com>
2026-05-14 11:47:38 -0700

    Added banana

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,14 @@
 export default function Home() {
   return (
     <div>
-      <h1 className="text-4xl font-bold text-blue-600">Salamander Tracker</h1>
-      <p>Pick a video from the Videos page to start analyzing.</p>
+      <h1 className="text-4xl font-bold  text-orange-400 text-center mt-2">
+        Salamander Tracker
+      </h1>
+      <p className="text-center mx-10">
+        Salamander Tracker is a project dedicated to tracking the location of
+        salamander through video. You upload a video and the API will track the
+        salamander. It will export an excel file of its locations every minute.
+      </p>
     </div>
   );
 }
