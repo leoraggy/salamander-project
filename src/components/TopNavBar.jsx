a6cfbcfe2444c4f55cdbb1d6272d5468f2e1a7a1
leoraggy <leoragual35@gmail.com>
2026-05-14 11:13:07 -0700

    Banana

--- src/components/TopNavBar.jsx
+++ src/components/TopNavBar.jsx
@@ -2,10 +2,20 @@ import { Link } from "react-router-dom";
 
 function TopNavBar() {
   return (
-    <nav>
-      <Link to="/">Home</Link>
-      {" | "}
-      <Link to="/videos">Videos</Link>
+    <nav className="flex justify-between  items-center p-5 bg-green-600">
+      <div>
+        <Link to="/" className="flex">
+          <img src="./logo.png" alt="Logo of Salamander" width={75} />
+          <p className="flex justify-center items-center text-white text-2xl pl-2 font-bold">
+            Salamander Project
+          </p>
+        </Link>
+      </div>
+      <div className="bg-orange-400 py-3 px-5 rounded-lg mr-2">
+        <Link to="/videos">
+          <p className="text-white text-2xl font-bold">Videos</p>
+        </Link>
+      </div>
     </nav>
   );
 }
