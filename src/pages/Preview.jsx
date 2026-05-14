e9c41d211683e9752884d92f2b5a65966da27656
kevinrsandoval <174657099+KevinSandoval12@users.noreply.github.com>
2026-05-14 13:19:05 -0700

    preview page styling

--- src/pages/Preview.jsx
+++ src/pages/Preview.jsx
@@ -4,12 +4,22 @@ export default function Preview() {
   const { filename } = useParams();
 
   return (
-    <div className="text-black">
-      <h1>Preview: {filename}</h1>
-      <p>
+    <div>
+      <div className="justify-self-center text-center font-medium pl-2 pr-2 m-2 bg-green-600 text-white w-auto rounded-sm">
+        <h1>Preview: {filename}</h1>
+        {/* <p>
         Thumbnail and tuning controls will go here in a future pair program.
-      </p>
-      <Link to="/videos">Back to videos</Link>
+      </p> */}
+        <Link to="/videos">Back to videos</Link>
+      </div>
+      <div className="flex justify-center space-x-8">
+        <div className="text-center bg-orange-300 text-white p-30 rounded-md">
+          Before Video
+        </div>
+        <div className="text-center bg-orange-300 text-white p-30 rounded-md">
+          After Video
+        </div>
+      </div>
     </div>
   );
 }
