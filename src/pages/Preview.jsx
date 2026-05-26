277c863b128bd097cf21ab30ac5fafd0a10eba9a
leoraggy <leoragual35@gmail.com>
2026-05-26 10:33:13 -0700

    Added thumbnail

--- src/pages/Preview.jsx
+++ src/pages/Preview.jsx
@@ -1,12 +1,35 @@
 import { Link, useParams } from "react-router-dom";
+import { getThumbnail } from "../mockApi";
+import { useState, useEffect } from "react";
 
 export default function Preview() {
   const { filename } = useParams();
 
+  const [thumbnail, setThumbnail] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const fetchThumbnail = async () => {
+    try {
+      const data = await getThumbnail(filename);
+      setThumbnail(data);
+    } catch (error) {
+      setError(error.message);
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchThumbnail();
+  }, []);
+
   return (
     <div>
       <div className="justify-self-center text-center font-medium pl-2 pr-2 m-2 bg-green-600 text-white w-auto rounded-sm">
-        <h1>Preview: {filename}</h1>
+        Preview: <img src={thumbnail} alt="image of salamander" />
+        {filename}
         {/* <p>
         Thumbnail and tuning controls will go here in a future pair program.
       </p> */}
